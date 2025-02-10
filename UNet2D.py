8379248e484a98b20afee15c5f41187668405ed2
Marcello <marcello.barylli@gmail.com>
2025-02-10 13:59:10 +0000

    implemented UNet2D

--- UNet2D.py
+++ UNet2D.py
@@ -1,3 +1,5 @@
+# %%
+
 import torchvision
 import torch
 from datasets import load_dataset
@@ -8,8 +10,9 @@ import torch
 import torch.nn.functional as F
 from matplotlib import pyplot as plt
 from PIL import Image
-from diffusers import DDPMScheduler
+from diffusers import DDPMScheduler, DDPMPipeline
 
+# %%
 
 def show_images(x):
     """Given a batch of images x, make a grid and convert to PIL"""
@@ -100,3 +103,56 @@ model.to(device)
 with torch.no_grad():
     model_prediction = model(noisy_xb, timesteps).sample
 model_prediction.shape
+
+
+### Minimal training loop, see https://colab.research.google.com/github/huggingface/notebooks/blob/main/diffusers/training_example.ipynb the official training example
+
+# Set the noise scheduler
+noise_scheduler = DDPMScheduler(num_train_timesteps=1000, beta_schedule="squaredcos_cap_v2")
+
+# Training loop
+optimizer = torch.optim.AdamW(model.parameters(), lr=4e-4)
+
+losses = []
+
+for epoch in range(30):
+    for step, batch in enumerate(train_dataloader):
+        clean_images = batch["images"].to(device)
+        # Sample noise to add to the images
+        noise = torch.randn(clean_images.shape).to(clean_images.device)
+        bs = clean_images.shape[0]
+
+        # Sample a random timestep for each image
+        timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
+
+        # Add noise to the clean images according to the noise magnitude at each timestep
+        noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+
+        # Get the model prediction
+        noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+
+        # Calculate the loss
+        loss = F.mse_loss(noise_pred, noise)
+        loss.backward(loss)
+        losses.append(loss.item())
+
+        # Update the model parameters with the optimizer
+        optimizer.step()
+        optimizer.zero_grad()
+
+    if (epoch + 1) % 5 == 0:
+        loss_last_epoch = sum(losses[-len(train_dataloader) :]) / len(train_dataloader)
+        print(f"Epoch:{epoch+1}, loss: {loss_last_epoch}")
+
+# %%
+# Plot the loss
+fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+axs[0].plot(losses)
+axs[1].plot(np.log(losses))
+plt.show()
+# %%
+image_pipe = DDPMPipeline(unet=model, scheduler=noise_scheduler)
+
+pipeline_output = image_pipe()
+pipeline_output.images[0]
+# %%
